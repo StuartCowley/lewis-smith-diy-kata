fa5996a58111898d46b93597e6ca55225f01edb8
Lewis Smith <lewislbssmith212@gmail.com>
2022-12-05 14:44:16 +0000

    Human Cat and Dog Years into Array

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -17,7 +17,7 @@ array of stringed numbers; initialised an empty array;
 use to forEach method to .push(+i).  The + signifies the 
 int operator should it precede the new array in the push function.
 
-Example
+Example forEach
 const numberToReversedDigits = (number) => {
     const stringArray = number.toString().split("").reverse();
     const numberArray = [];
@@ -25,3 +25,18 @@ const numberToReversedDigits = (number) => {
     return numberArray;
 }
 /*
+/* Example Reduce
+const numberToReversedDigits = (number) => {
+    const stringArray = number.toString().split("").reverse();
+    let numberArray = stringArray.reduce((acc, x) => acc.concat(+x), []);
+    return numberArray;
+}
+
+Breakdown of the reduce method:
+    You want to reduce the string array (using variables 
+        acc for accumulator, and x for its current index value.
+        Then, the function itself stipulates to take the value of acc
+        and concatenate it, in and of itself, takes each element, and
+        squeezes them together inside one individual string.  From there
+        the (+x), [] then parses it as an integer, and pushes into
+        an array, respectively */
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,11 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  test("returns human, cat, and dog yeats in an array", () => {
+    expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
+    expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+    expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+    expect(humanCatDogYears(5)).toEqual([5, 36, 39]);
+    expect(humanCatDogYears(31)).toEqual([31, 140, 169]);
+  });
+});
