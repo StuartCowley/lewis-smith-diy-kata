5464f33bdabb4b2bca30c100cff55ba11a839ccb
Lewis Smith <lewislbssmith212@gmail.com>
2022-12-05 15:14:38 +0000

    Reach Destination Plural and Singular String with Time Variable

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,12 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  let time = distance / speed;
+  time = Math.round(time * 2) / 2;
+  const message1 = `I should be there in ${time} hours.`;
+  const message2 = `I should be there in ${time} hour.`;
+  if (time === 1) {
+    return message2;
+  }
+  return message1;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,11 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(30, 15)).toEqual("I should be there in 2 hours.");
+    expect(reachDestination(60, 45)).toEqual("I should be there in 1.5 hours.");
+    expect(reachDestination(60, 15)).toEqual("I should be there in 4 hours.");
+    expect(reachDestination(12, 45)).toEqual("I should be there in 0.5 hours.");
+    expect(reachDestination(22.5, 30)).toEqual("I should be there in 1 hour.");
+  });
 });
