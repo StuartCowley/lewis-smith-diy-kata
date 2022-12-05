202d1131fc01d0221db9a494bc1f683e972e8a06
Lewis Smith <lewislbssmith212@gmail.com>
2022-12-05 12:33:18 +0000

    Another example on how to achieve this function result

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -5,3 +5,23 @@ const numberToReversedDigits = (number) => {
 };
 
 module.exports = numberToReversedDigits;
+
+
+/*
+You could use an for loop to iterate over each individual
+numbered string, and then parseint onto each index; followed
+by pushing onto an empty array.
+
+You could use a forEach loop - you'd ensure you have your 
+array of stringed numbers; initialised an empty array;
+use to forEach method to .push(+i).  The + signifies the 
+int operator should it precede the new array in the push function.
+
+Example
+const numberToReversedDigits = (number) => {
+    const stringArray = number.toString().split("").reverse();
+    const numberArray = [];
+    stringArray.forEach(i => numberArray.push(+i));
+    return numberArray;
+}
+/*
